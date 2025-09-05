36f72613beffb3bdf76c30df73e44697e5ad044a
khalilv63-cyber <khalilv63@gmail.com>
2025-09-05 04:39:35 +0100

    main.cpp
    main.cp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+  int sum;
+
+  cout << "Enter Number One : ";
+  cin >> num1;
+
+  cout << "Enter Number Two : ";
+  cin >> num2;
+
+  sum = num1 + num2;
+
+  cout << num1 << " + " << num2 << " = " << sum << endl;
+  return 0;
+}
